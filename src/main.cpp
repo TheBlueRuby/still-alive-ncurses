2fda2c00e4423811db580b09a7245076de1cd3df
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-12-21 20:17:37 +1100

    added basic lyric printing

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
 #include <curses.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
+#include <chrono>
+#include <thread>
+
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
+
+void lyricprint(const char* line);
 
 int main(void)
 {
     initscr();
 
-    addstr("Still Alive with NCurses");
+    addstr("Still Alive with NCurses\n");
     refresh();
+
+    lyricprint("This was a triumph");
 
     getch();
     endwin();
     return EXIT_SUCCESS;
+}
+
+void lyricprint(const char* line)
+{
+    for (unsigned int i = 0; i < strlen(line); i++)
+    {
+        addch(line[i]);
+        refresh();
+        sleep_for(milliseconds(50));
+    }
+    return;
 }
