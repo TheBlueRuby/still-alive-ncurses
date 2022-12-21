71f8ec2e6fb3d5c7761ee4a9dbfb99be15b9d710
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-12-21 20:28:39 +1100

    moved init logic into separate function

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,15 @@
 #include <chrono>
 #include <thread>
 
-using std::this_thread::sleep_for;
 using std::chrono::milliseconds;
+using std::this_thread::sleep_for;
 
-void lyricprint(const char* line);
+void init();
+void lyricprint(const char *line);
 
 int main(void)
 {
-    initscr();
+    init();
 
     addstr("Still Alive with NCurses\n");
     refresh();
@@ -23,7 +24,31 @@ int main(void)
     return EXIT_SUCCESS;
 }
 
-void lyricprint(const char* line)
+void init()
+{
+    initscr();
+
+    if (has_colors())
+    {
+        if (start_color() == OK)
+        {
+            init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+            attrset(COLOR_PAIR(1));
+        }
+        else
+        {
+            addstr("Could not init colors!\n");
+            refresh();
+        }
+    }
+    else
+    {
+        addstr("No colors capable!\n");
+        refresh();
+    }
+}
+
+void lyricprint(const char *line)
 {
     for (unsigned int i = 0; i < strlen(line); i++)
     {
