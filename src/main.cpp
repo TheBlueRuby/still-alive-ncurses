6890a78539758f21905c26c56fd0deec9ce94e67
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-12-24 12:36:43 +1100

    added audio finished first lyric screen and synced

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <curses.h>
 #include <cstdlib>
+#include <cstdio>
 #include <cstring>
+
+#define MINIAUDIO_IMPLEMENTATION
+#include "../includes/miniaudio.h"
 
 void initWindow(WINDOW *win);
 void lyrics(WINDOW *lyricWin);
+void lyricprint(const char *line, WINDOW *lyricWin, int row, int napMs, int linewaitms);
 void lyricprint(const char *line, WINDOW *lyricWin, int row);
 
 int main(void)
@@ -11,13 +16,36 @@ int main(void)
     // ncurses init
     initscr();
 
+    // sound init
+    ma_result maResult;
+    ma_engine maEngine;
+
+    FILE *audioFile;
+    if (!(audioFile = fopen("stillalive.mp3", "r")))
+    {
+        printf("File not found!");
+        return 2;
+    }
+
+    maResult = ma_engine_init(NULL, &maEngine);
+    if (maResult != MA_SUCCESS)
+    {
+        printf("Could not init audio!");
+        return -1;
+    }
+
+
     WINDOW *lyricWin = newwin(28, 48, 0, 0);
     initWindow(lyricWin);
 
+    ma_engine_play_sound(&maEngine, "stillalive.mp3", NULL);
     lyrics(lyricWin);
 
     getch();
+
     endwin();
+    ma_engine_uninit(&maEngine);
+
     return EXIT_SUCCESS;
 }
 
@@ -54,27 +82,52 @@ void lyrics(WINDOW *lyricWin)
     int row = 0;
 
     row++;
+    lyricprint("Forms FORM-29827281-12:", lyricWin, row, 30, 3000);
+    row++;
+    lyricprint("Test Assessment Report", lyricWin, row, 30, 3000);
+    row++;
+    row++;
+    row++;
+
+    row++;
     lyricprint("This was a triumph.", lyricWin, row);
     row++;
     lyricprint("I'm making a note here:\n HUGE SUCCESS", lyricWin, row);
     row++;
     row++;
-    lyricprint("I'ts hard to overstate my satisfaction.", lyricWin, row);
-    row++;
-
+    lyricprint("I'ts hard to overstate my satisfaction.", lyricWin, row, 100, 2500);
     row++;
     lyricprint("Aperture Science.", lyricWin, row);
     row++;
-    lyricprint("We do what we must, because we can.", lyricWin, row);
+    lyricprint("We do what we must\n because we can.", lyricWin, row);
     row++;
-    lyricprint("For the good of all of us,", lyricWin, row);
     row++;
-    lyricprint("Except the ones who are dead.", lyricWin, row);
+    lyricprint("For the good of all of us,", lyricWin, row, 100, 1300);
+    row++;
+    lyricprint("Except the ones who are dead.", lyricWin, row, 50, 500);
+    row++;
+    napms(1000);
+    row++;
+    lyricprint("But there's no sense crying\n over every mistake.", lyricWin, row, 50, 750);
+    row++;
+    row++;
+    lyricprint("You just keep on trying\n till you run out of cake.", lyricWin, row, 50, 750);
+    row++;
+    row++;
+    lyricprint("And the Science gets done.", lyricWin, row, 50, 750);
+    row++;
+    lyricprint("And you make a neat gun.", lyricWin, row, 50, 750);
+    row++;
+    lyricprint("For the people who are.", lyricWin, row, 50, 750);
+    row++;
+    lyricprint("still alive.", lyricWin, row);
+    row++;
+
 
     return;
 }
 
-void lyricprint(const char *line, WINDOW *lyricWin, int row)
+void lyricprint(const char *line, WINDOW *lyricWin, int row, int napMs, int linewaitms)
 {
     wmove(lyricWin, row, 1);
     for (unsigned int i = 0; i < strlen(line); i++)
@@ -82,10 +135,15 @@ void lyricprint(const char *line, WINDOW *lyricWin, int row)
         waddch(lyricWin, line[i]);
         box(lyricWin, '|', '-');
         wrefresh(lyricWin);
-        napms(100);
+        napms(napMs);
     }
 
-    napms(2000);
+    napms(linewaitms);
 
     return;
+}
+
+void lyricprint(const char *line, WINDOW *lyricWin, int row)
+{
+    lyricprint(line, lyricWin, row, 100, 2000);
 }
